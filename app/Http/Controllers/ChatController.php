258552fe74d61239ae96bdc5ac7bbf42448f4432
rabiulhasan3 <rabiul.fci@gmail.com>
2019-03-19 17:09:50 +0600

    event and auth created

---
+++ app/Http/Controllers/ChatController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\User;
+use Auth;
+use App\Events\ChatEvent;
+
+class ChatController extends Controller
+{
+        /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+
+    public function chat(){
+        
+        return view('chat');
+    }
+
+    public function send(Request $request){
+        $user = User::find(Auth::id());
+
+        event(new ChatEvent($request->message,$user));
+
+    }
+
+}
--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('chat',function(){
-    return view('chat');
-});
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('chat','ChatController@chat');
+Route::post('send','ChatController@send');
+
